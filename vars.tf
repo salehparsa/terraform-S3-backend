390a64c82369072039f0e83744e9d8a59984a68d
Saleh Parsa <salehp@gmail.com>
2018-12-28 11:57:44 +0100

    fixed format

--- vars.tf
+++ vars.tf
@@ -1,9 +1,9 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket. Must be globally unique."
-  default = "aws-backend-state-bucket-salehparsa"
+  default     = "aws-backend-state-bucket-salehparsa"
 }
 
 variable "aws_region" {
   description = "The AWS region to use"
-  default = "eu-central-1"
+  default     = "eu-central-1"
 }
